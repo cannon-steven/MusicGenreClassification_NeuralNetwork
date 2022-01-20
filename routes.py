7eb2bd672211537d230e93d1c493b38e8b76767a
cannon-steven <cannon.j.steven@gmail.com>
2022-01-19 16:24:41 -0800

    Moved routes and other folders out of app/ and back into main folder

---
+++ routes.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from app import app
+
+#--- WEB PAGES ---
+
+@app.route("/", methods=["GET"])
+def start():
+    return render_template('start.html')
+
+
+
+# --- BACKEND API ---
+@app.route("/songs", methods=["POST"])
+def songs():
+    return "Nothing"
