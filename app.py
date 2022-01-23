834feab6495ccc687593cac0f4921356252f9e86
cannon-steven <cannon.j.steven@gmail.com>
2022-01-23 12:37:41 -0800

    Linted app.py

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
-#--- WEB PAGES ---
+# --- WEB PAGES ---
+
 
 @app.route("/", methods=["GET"])
 def start():
     return render_template('start.html')
 
 
-
 # --- BACKEND API ---
 @app.route("/songs", methods=["POST"])
 def songs():
     return "Nothing"
-
