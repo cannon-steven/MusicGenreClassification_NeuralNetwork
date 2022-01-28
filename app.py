c148683b972e61cfd11cab4be992e349cb0c31e8
cannon-steven <cannon.j.steven@gmail.com>
2022-01-27 18:25:19 -0800

    added GET route to main page

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, Response, render_template, jsonify
 
+ALLOWED_EXTENSIONS = {'wav'}
 
 app = Flask(__name__)
 
+
+# --- HELPER FUNCTIONS ---
+def is_allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 # --- WEB PAGES ---
-
-
-@app.route("/", methods=["GET"])
+@app.route("/")
 def start():
     return render_template('start.html')
 
 
-# --- BACKEND API ---
-@app.route("/songs", methods=["POST"])
-def songs():
+@app.route("/main")
+def main_web_page():
     return render_template('main.html')
+
+
+# --- BACKEND API ---
+
