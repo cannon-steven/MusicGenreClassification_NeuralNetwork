663c2e3edab22b2c6c175a2f6fb2c79359239baf
cannon-steven <cannon.j.steven@gmail.com>
2022-01-28 17:04:19 -0800

    Added script to only reveal selected results

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,47 @@
 function uploadFile(){
     document.getElementById("upload").click()
 }
+
+
+function parseFileName(str){
+    return str.slice(0, -3) // Assumes string is of format "filename_li"
+}
+
+
+function getMax(jsonArr){
+    let max;
+    for (element in jsonArr){
+        if (max == null || parseInt(jsonArr[element]) > parseInt(jsonArr[max])){
+            max = element
+        }
+    }
+    return max
+}
+
+
+function showResults(fileName, resultPages){
+    // Hide all results pages
+    for (page of resultPages){
+        page.style.display = "none"
+    }
+    // Hide "none selected" page
+    document.getElementById("noSelection").style.display = "none"
+    // Reveal selected results page
+    document.getElementById(fileName + "_results").style.display = "flex"
+}
+
+// Get arrays of DOM elements
+var songList = document.getElementsByClassName("songList")
+var resultPages = document.getElementsByClassName("results")
+
+// Add event listeners to the song list to display results when clicked
+for (song of songList){
+    let fileName = parseFileName(song.id)
+    song.onclick = function(){showResults(fileName, resultPages)}
+}
+
+
+
+// function updateSongList(){
+//     fetch
+// }
