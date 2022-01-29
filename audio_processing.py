68822f74d62e1839afa26af700ac4e0024a0a53f
Mateo Estrada <mateoej12@gmail.com>
2022-01-29 14:44:38 -0800

    Added the processing code for the genres training and the single file processing will be added here, still need to test that with single files.

---
+++ audio_processing.py
@@ -0,0 +1,56 @@
+import librosa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+import librosa.feature
+# Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+#Keras
+import keras
+
+def make_dataset(header):
+    file = open('data.csv', 'w', newline='')
+    with file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+    genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+    for g in genres:
+        if os.listdir(f'./single_audio.wav'):
+            songname = f'./single_audio.wav'
+        else:
+            for filename in os.listdir(f'./genres/{g}'):
+                songname = f'./genres/{g}/{filename}'
+                y, sr = librosa.load(songname, mono=True, duration=30)
+                chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+                rmse = librosa.feature.rmse(y=y)
+                spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+                spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+                rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+                zcr = librosa.feature.zero_crossing_rate(y)
+                mfcc = librosa.feature.mfcc(y=y, sr=sr)
+                to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+                for e in mfcc:
+                    to_append += f' {np.mean(e)}'
+                to_append += f' {g}'
+                file = open('data.csv', 'a', newline='')
+                with file:
+                    writer = csv.writer(file)
+                    writer.writerow(to_append.split())
+            
+
+# generating a dataset
+def init_dataset_header():
+    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+    for i in range(1, 21):
+        header += f' mfcc{i}'
+    header += ' label'
+    header = header.split()
+    make_dataset(header)
+    
+    
+if __name__=='__main__':
+    init_dataset_header()
+    
