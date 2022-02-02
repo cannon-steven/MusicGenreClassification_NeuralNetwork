fa3df11ef15fd3cacbf2b679952029bffef081ff
Mateo Estrada <mateoej12@gmail.com>
2022-02-01 17:36:43 -0800

    Went through the code to fix linting and removed unused libraries.

--- audio_processing.py
+++ audio_processing.py
@@ -1,30 +1,28 @@
 import librosa
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import csv
 import librosa.feature
-# Preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-#Keras
-import keras
+
 
 def make_dataset(header):
     '''
     Takes the header values that will be in the data.csv file.
-    Opens a new file and populates the header and the rows using the generes (GTZAN) dataset via librosa.
+    Opens a new file and populates the header and the rows using
+    the generes (GTZAN) dataset via librosa.
     '''
     # open the file and write the first row with the header attributes
     file = open('data.csv', 'w', newline='')
     with file:
         writer = csv.writer(file)
         writer.writerow(header)
-    # These are the genres in the GTZAN directories and we are going to loop through those directories to get the wav files.
-    genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+    # These are the genres in the GTZAN directories
+    # and we are going to loop through those directories to get the wav files.
+    genres = 'blues classical country disco'
+    'hiphop jazz metal pop reggae rock'.split()
     for g in genres:
-        # loop through the genres directory and pick the wav file from each genre directory
+        # loop through the genres directory
+        # and pick the wav file from each genre directory
         for filename in os.listdir(f'./genres/{g}'):
             # songname is the .wav filename
             songname = f'./genres/{g}/{filename}'
@@ -45,8 +43,11 @@ def make_dataset(header):
             # The Mel-Frequency Cepstral Coefficients (21 in our case)
             mfcc = librosa.feature.mfcc(y=y, sr=sr)
             # take the mean of each attribute and append it as a string
-            to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
-            # loop through all the mfcc values and append them together to add them on the to_append variable
+            to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)}'
+            '{np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)}'
+            '{np.mean(zcr)}'
+            # loop through all the mfcc values and append them
+            # together to add them on the to_append variable
             for e in mfcc:
                 to_append += f' {np.mean(e)}'
             to_append += f' {g}'
@@ -55,16 +56,16 @@ def make_dataset(header):
             with file:
                 writer = csv.writer(file)
                 writer.writerow(to_append.split())
-            
 
 
 def init_dataset_header():
     '''
     Initializes the header for the data.csv file.
-    Uses the make_dataset() function to generate the data.csv file. 
+    Uses the make_dataset() function to generate the data.csv file.
     '''
-    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
-    
+    header = 'filename chroma_stft '
+    'rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+
     # I need to generate 21 columns in the csv file for the mfcc values
     for i in range(1, 21):
         header += f' mfcc{i}'
@@ -72,8 +73,7 @@ def init_dataset_header():
     header = header.split()
     # Use the function and pass it the header
     make_dataset(header)
-    
-    
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     init_dataset_header()
-    
