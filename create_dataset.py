0aa514d8819923962b4669270b6b26d7fa3455eb
cannon-steven <cannon.j.steven@gmail.com>
2022-02-11 18:38:15 -0800

    Added command line arguments for create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,17 @@
+import sys
 import SpotifyPreviews as sp
+
+# Creates a .csv file filled with feature data extracted from song clips on
+# Spotify. Quantity is the number of songs in each genre you would like to add
+# to the file.
+
+# Verify presence of quantity argument
+if len(sys.argv) != 2:
+    print("Usage: create_dataset.py <quantity>")
+    sys.exit()
+
+quantity = int(sys.argv[1])
 
 genres = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal",
           "pop", "reggae", "rock"]
-sp.collect_spotify_data(genres, 1000)
+sp.collect_spotify_data(genres, quantity)
