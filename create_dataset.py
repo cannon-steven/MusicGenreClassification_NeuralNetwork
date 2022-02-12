019e2349562a1c50b0cf1865a5ea9f769a2acc89
cannon-steven <cannon.j.steven@gmail.com>
2022-02-11 17:44:57 -0800

    Added script to create a new dataset

---
+++ create_dataset.py
@@ -0,0 +1,5 @@
+import SpotifyPreviews as sp
+
+genres = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal",
+          "pop", "reggae", "rock"]
+sp.collect_spotify_data(genres, 1000)
