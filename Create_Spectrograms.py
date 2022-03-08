51d801954ca44aa4288047dfa3c08d85aac67592
cannon-steven <cannon.j.steven@gmail.com>
2022-03-07 21:27:11 -0800

    Re-updated Create_Spectrograms since it got lost somehow

--- Create_Spectrograms.py
+++ Create_Spectrograms.py
@@ -3,6 +3,7 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
 
 
 def build_empty_directories(outputDirectory, labels):
@@ -41,12 +42,49 @@ def save_spectrogram(songPath, destination):
 
     # Get song representation as a spectrogram
     y, sr = librosa.load(songPath)
+    plt.axis("off")  # Needed to remove white border from saved image.
     data = librosa.stft(y)
     S_db = librosa.amplitude_to_db(np.abs(data), ref=np.max)
     librosa.display.specshow(S_db)
 
     # Save file to destination
-    plt.savefig(f"{destination}/{filename}")
+    plt.savefig(
+        f"{destination}/{filename}",
+        bbox_inches="tight",  # This and pad_inches and plt.axis("off") remove
+        pad_inches=0.0        # the white borders from the image
+    )
+
+    # Clear current axis. Without this the time to save increases with
+    # each iteration
+    plt.cla()
+
+
+def save_mel_spectrogram(songPath, destination):
+    """
+    Given the path to a .wav file and a destination directory, saves a png
+    image of the spectrogram to the directory
+    """
+    # Get filename
+    filename = parse_file_name(songPath)
+    filename += "_mel.png"
+
+    # Get song representation as a spectrogram
+    y, sr = librosa.load(songPath)
+    plt.axis("off")  # Needed to remove white border from saved image.
+    mel = librosa.feature.melspectrogram(y=y, sr=sr)
+    S_dB = librosa.power_to_db(mel, ref=np.max)
+    librosa.display.specshow(S_dB)
+
+    # Save file to destination
+    plt.savefig(
+        f"{destination}/{filename}",
+        bbox_inches="tight",  # This and pad_inches and plt.axis("off") remove
+        pad_inches=0.0        # the white borders from the image
+    )
+
+    # Clear current axis. Without this the time to save increases with
+    # each iteration
+    plt.cla()
 
 
 def generate_spectrograms(inputDirectory, outputDirectory):
@@ -67,9 +105,35 @@ def generate_spectrograms(inputDirectory, outputDirectory):
         for song in os.listdir(f"{inputDirectory}/{genre}"):
             file_png = parse_file_name(song) + ".png"
             if file_png not in os.listdir(f"{outputDirectory}/{genre}"):
-                print(f"Generating spectrogram for {song}")
+                startTime = time.time()
+                print(f"Generating spectrogram for {song} ", end="")
                 save_spectrogram(f"{inputDirectory}/{genre}/{song}",
                                  f"{outputDirectory}/{genre}")
+                print("- Time taken = ", (time.time() - startTime))
 
 
-generate_spectrograms("./Data/genres_original", "./Spectrograms")
+def generate_mel_spectrograms(inputDirectory, outputDirectory):
+    """
+    Given the GTZAN genres dataset, creates mel spectrograms and fills a
+    training directory
+    """
+    # Get list of genres
+    genres = os.listdir(inputDirectory)
+
+    # Create empty directories to put the spectrograms into
+    dirName = parse_directory(outputDirectory)
+    if dirName not in os.listdir("./"):
+        build_empty_directories(outputDirectory, genres)
+
+    # Get songs and convert them to spectrograms
+    for genre in genres:
+        for song in os.listdir(f"{inputDirectory}/{genre}"):
+            file_png = parse_file_name(song) + ".png"
+            if file_png not in os.listdir(f"{outputDirectory}/{genre}"):
+                startTime = time.time()
+                print(f"Generating mel spectrogram for {song} ", end="")
+                save_mel_spectrogram(
+                    f"{inputDirectory}/{genre}/{song}",
+                    f"{outputDirectory}/{genre}"
+                )
+                print("- Time taken = ", (time.time() - startTime))
